50470663c7402a8776bf67806230f34b177fbe01
Brad <bjkapteyn@gmail.com>
2020-07-09 16:25:26 -0400

    cleared node if you click a different card

--- FlashGordon/wwwroot/js/createForm.js
+++ FlashGordon/wwwroot/js/createForm.js
@@ -6,6 +6,7 @@ function createForm(formID) {
     let cardData = grabText(formID);
     //turn it into a number to submit
     let idInt = parseInt(formID);
+
     let form = document.createElement("form");
     let frontCardLabel = document.createElement("p");
     let frontCardInput = document.createElement("input");
@@ -14,11 +15,9 @@ function createForm(formID) {
     let categoryLabel = document.createElement("p");
     let categoryInput = document.createElement("input");
     let submitButton = document.createElement("button");
-
     let itemArray = [frontCardLabel, frontCardInput, backCardLabel, backCardInput, categoryLabel,categoryInput, submitButton];
 
     form.id = "updateForm";
-
     frontCardLabel.innerText = "Front";
     frontCardLabel.id = "formFront";
     frontCardInput.type = "text";
@@ -34,13 +33,17 @@ function createForm(formID) {
     //turn this into radio later---------------------------------------------------TODO
     categoryInput.type = "text";
     categoryInput.value = cardData.Category;
+    //clear all previous nodes
+    if (idInt != formID) {
+        while (nodeStart.firstChild) {
+            nodeStart.removeChild(nodeStart.lastChild);
+        }
+    }
 
     for (i = 0; i < itemArray.length; i++) {
         form.appendChild(itemArray[i]);
     }
-
     nodeStart.appendChild(form);
-
 }
 
 //takes text form the card element returns it
