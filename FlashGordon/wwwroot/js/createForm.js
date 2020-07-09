c07ce9d7053e1eaab17e5fb1bfbf3f5be71fb643
Brad <bjkapteyn@gmail.com>
2020-07-09 15:52:02 -0400

    added javascript function for building a form out of each card for the update part

--- FlashGordon/wwwroot/js/createForm.js
+++ FlashGordon/wwwroot/js/createForm.js
@@ -1,7 +1,64 @@
-﻿function createForm(formID) {
+﻿
+//refactor me please--------------------------------------------------TODO
+function createForm(formID) {
+
+    let nodeStart = document.getElementById("formStandin");
+    let cardData = grabText(formID);
+    //turn it into a number to submit
+    let idInt = parseInt(formID);
+    let form = document.createElement("form");
+    let frontCardLabel = document.createElement("p");
+    let frontCardInput = document.createElement("input");
+    let backCardLabel = document.createElement("p");
+    let backCardInput = document.createElement("input");
+    let categoryLabel = document.createElement("p");
+    let categoryInput = document.createElement("input");
+    let submitButton = document.createElement("button");
+
+    let itemArray = [frontCardLabel, frontCardInput, backCardLabel, backCardInput, categoryLabel,categoryInput, submitButton];
+
+    form.id = "updateForm";
+
+    frontCardLabel.innerText = "Front";
+    frontCardLabel.id = "formFront";
+    frontCardInput.type = "text";
+    frontCardInput.value = cardData.Front;
+
+    backCardLabel.innerText = "Back";
+    backCardLabel.id = "formBack";
+    backCardInput.type = "text";
+    backCardInput.value = cardData.Back;
+
+    categoryLabel.innerText = "Category";
+    categoryLabel.id = "formCategory";
+    //turn this into radio later---------------------------------------------------TODO
+    categoryInput.type = "text";
+    categoryInput.value = cardData.Category;
+
+    for (i = 0; i < itemArray.length; i++) {
+        form.appendChild(itemArray[i]);
+    }
+
+    nodeStart.appendChild(form);
+
+}
+
+//takes text form the card element returns it
+function grabText(formID) {
     let startPoint = document.getElementById(formID);
+    let startPointKids = startPoint.children;
+    let otherKids = startPointKids[1].children;
 
-    let children = startPoint.children;
+    let category = startPointKids[0].innerText;
+    let frontCard = otherKids[1].innerText;
+    let backCard = otherKids[3].innerText;
 
-    console.log(children);
+    //Assign the data names
+    let data = {
+        Category: category,
+        Front: frontCard,
+        Back: backCard
+    }
+
+    return data;
 }
